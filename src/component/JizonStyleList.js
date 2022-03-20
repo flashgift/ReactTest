ac8d597fb5799bac80235b2acaa38e7ca03ee16a
flashgift <flashgift80@gmail.com>
2022-03-20 13:29:34 +0900

    list done

--- src/component/JizonStyleList.js
+++ src/component/JizonStyleList.js
@@ -11,45 +11,43 @@ const JizonStyleList = (props) => {
     const [isTreeExpand, setIsTreeExpand] = useState(true);
     const ref4ChildHeight = useRef();
 
-    const animationDone = () => {
-        if (isTreeExpand) {
-            ref4ChildHeight.current.style.height = 'auto';
-        } else {
-            ref4ChildHeight.current.style.height = '0px';
+    const animationDone = (e) => {
+        if (e.target === ref4ChildHeight.current) {
+            if (isTreeExpand) {
+                ref4ChildHeight.current.style.height = 'auto';
+            }
         }
     };
 
-    const setListStyle = () => {
+    const onListClick = () => {
         if (ref4ChildHeight.current !== undefined) {
             let resultheight = 0;
             for (let item of ref4ChildHeight.current.children) {
                 resultheight += item.scrollHeight;
             }
-
             if (isTreeExpand) {
-                setTimeout(() => { ref4ChildHeight.current.style.height = resultheight + "px"; }, 10);
-                ref4ChildHeight.current.style.height = "0px";
-            } else {
+                setIsTreeExpand(false);
                 setTimeout(() => { ref4ChildHeight.current.style.height = "0px"; }, 10);
                 ref4ChildHeight.current.style.height = resultheight + "px";
+            } else {
+                setIsTreeExpand(true);
+                setTimeout(() => { ref4ChildHeight.current.style.height = resultheight + "px"; }, 10);
+                ref4ChildHeight.current.style.height = "0px";
             }
         }
     };
 
-    useEffect(() => {
-        setListStyle();
-    });
 
     return (
         <ul className={styles.listStyle}>
             <span>
-                <span className={styles.treeExpandIcon + " " + (isTreeExpand ? styles.treeExpandIconOn : styles.treeExpandIconOff)} onClick={() => setIsTreeExpand(!isTreeExpand)}></span>
+                <span className={styles.treeExpandIcon + " " + (isTreeExpand ? styles.treeExpandIconOn : styles.treeExpandIconOff)} onClick={onListClick}></span>
                 <span className={styles.ItemLegendIcon}></span>
                 <span className={styles.elementName}>
                     {props.name}
                 </span>
             </span>
-            <ul onTransitionEnd={animationDone} ref={ref4ChildHeight}>
+            <ul className="itemList" onTransitionEnd={animationDone} ref={ref4ChildHeight}>
                 {props.children}
             </ul>
         </ul>
